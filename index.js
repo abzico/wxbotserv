24ebfdb018fd2999e35c9ae88960f99153cee192
Wasin Thonkaew <haxpor@gmail.com>
2017-11-16 05:13:39 +0800

    Clean up, and remove jsdom

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const req = require('request-promise-native');
 const phantom = require('phantom');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-let dom = null;
 let _ph, _page;
 
 phantom.create()
@@ -27,12 +24,6 @@ phantom.create()
 		_page.on('onResourceReceived', (resource) => {
 			console.log(resource);
 		});
-		// receive
-		// _page.on('onResourceReceived', (resource) => {
-		// 	if (resource.redirectURL) {
-		// 		console.log('redirect:' + resource.redirectURL);
-		// 	}
-		// });
 
 		return _page.open('https://wx.qq.com');
 	})
@@ -40,97 +31,9 @@ phantom.create()
 		console.log(status);
 		return _page.property('content');
 	})
-	// .then((content) => {
-	// 	//console.log(content);
-
-	// 	// prepare url for opening
-	// 	let reqTimestamp = Date.now();
-	// 	let redirectUri = 'https%3A%2F%2Fwx.qq.com%2Fcgi-bin%2Fmmwebwx-bin%2Fwebwxnewloginpage&fun=new&lang=en_US&_=' + reqTimestamp;
-
-	// 	// open login page
-	// 	return _page.open('https://login.wx.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=' + redirectUri);
-	// })
-	// .then((status) => {
-	// 	console.log(status);
-	// 	return _page.property('content');
-	// })
-	// .then((content) => {
-	// 	// get result of QRCode url
-	// 	console.log(content);
-
-	// 	var qrcodeImageUrl = 'https://login.weixin.qq.com/qrcode/' + /window.QRLogin.uuid = \"(.+?)\";/.exec(content)[1];
-	// 	console.log('Got qrcode image url: ' + qrcodeImageUrl);
-	// 	// notify image to slack
-	// 	notifyViaSlack(qrcodeImageUrl);
-	// })
 	.catch((err) => {
 		console.log(err);
 	})
-
-// let options = {
-// 		method: 'GET',
-// 		uri: 'https://wx.qq.com',
-// 		headers: {
-// 			'Connection': 'Keep-Alive'
-// 		}
-// 	};
-
-// 	req(options)
-// 		.then((html) => {
-// 			dom = new JSDOM(html, { runScripts: "dangerously" });
-// 			printQRCodeImageUrl(dom);
-// 			cont();
-// 		})
-// 		.catch((err) => {
-// 			console.log(err);
-// 		});
-
-function printQRCodeImageUrl(dom) {
-	let document = dom.window.document;
-	let mmsrc = document.getElementsByClassName('qrcode')[0].getElementsByClassName('img')[0].getAttribute('mm-src');
-	console.log(mmsrc);
-}
-
-function cont() {
-	
-	let options = {
-		method: 'GET',
-		uri: 'https://login.wx.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=' + redirectUri,
-		headers: {
-			'Connection': 'Keep-Alive'
-		}
-	};
-
-	// make a request to WeChat's base login URL
-	req(options)
-		.then((res) => {
-			// form qrcode image url
-			var qrcodeImageUrl = 'https://login.weixin.qq.com/qrcode/' + /window.QRLogin.uuid = \"(.+?)\";/.exec(res)[1];
-
-			// make a request back to original
-			let options = {
-				method: 'GET',
-				uri: redirectUri,
-				headers: {
-					'Connection': 'Keep-Alive'
-				}
-			};
-			req(options)
-				.then((html) => {
-					dom = new JSDOM(html, { runScripts: "dangerously" } );
-					printQRCodeImageUrl(dom);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-
-			// notify image to slack
-			notifyViaSlack(qrcodeImageUrl);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-}
 
 function notifyViaSlack(qrcodeImageUrl) {
 	// notify via slack if SLACK_WEBHOOK_URL is set
